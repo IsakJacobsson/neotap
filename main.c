09d591cccbd71cea6f8ffc013a215a008df2d399
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-09-11 20:28:04 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,295 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <termios.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define NBR_WORDS 1000    // words to read
+#define MAX_WORD_LEN 100  // maximum length of each word
+#define NBR_TEXT_WORDS 10 // words in text
+#define NUM_KEYS 25       // a to y
+
+typedef struct
+{
+    char key;
+    int successes;
+    int fails;
+    double total_time;
+    int attempts;
+} key_stat;
+
+void load_key_stats(const char *filename, key_stat key_stats[])
+{
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+    {
+        perror("Could not open file");
+        exit(1);
+    }
+
+    char key;
+    int successes, fails;
+    double total_time;
+
+    while (fscanf(fp, " %c %d %d %lf", &key, &successes, &fails, &total_time) == 4)
+    {
+        int idx = key - 'a';
+        key_stats[idx].key = key;
+        key_stats[idx].successes = successes;
+        key_stats[idx].fails = fails;
+        key_stats[idx].total_time = total_time;
+    }
+
+    fclose(fp);
+}
+
+void save_key_stats(const char *filename, key_stat key_stats[])
+{
+    FILE *fp = fopen(filename, "w");
+    if (!fp)
+    {
+        perror("Could not open file");
+        exit(1);
+    }
+    for (int i = 0; i < NUM_KEYS; i++)
+    {
+        fprintf(fp, "%c %d %d %lf\n", key_stats[i].key, key_stats[i].successes, key_stats[i].fails, key_stats[i].total_time);
+    }
+    fclose(fp);
+}
+
+int read_words(const char *filename, char *words[], int max_words)
+{
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+    {
+        perror("Could not open file");
+        return -1;
+    }
+
+    char buffer[MAX_WORD_LEN];
+    int count = 0;
+
+    while (fgets(buffer, sizeof(buffer), fp) && count < max_words)
+    {
+        buffer[strcspn(buffer, "\n")] = '\0'; // remove newline
+        words[count] = strdup(buffer);        // allocate and copy
+        if (!words[count])
+        {
+            perror("Memory allocation failed");
+            break;
+        }
+        count++;
+    }
+
+    fclose(fp);
+    return count;
+}
+
+void build_test_text(
+    char *words[],
+    size_t num_words,
+    char *output,
+    size_t output_size,
+    size_t num_test_words)
+{
+    if (output_size == 0)
+        return;
+
+    size_t current_idx = 0;
+
+    for (size_t i = 0; i < num_test_words; i++)
+    {
+        int word_idx = rand() % num_words;
+        char *word = words[word_idx];
+        size_t word_len = strlen(word);
+
+        // Check if the word fits in the remaining buffer (including space or null)
+        size_t space_needed = word_len;
+        if (i > 0)
+            space_needed += 1; // space before word if not first
+
+        if (current_idx + space_needed >= output_size)
+        {
+            break; // not enough space
+        }
+
+        // Add space if not the first word
+        if (i > 0)
+        {
+            output[current_idx++] = ' ';
+        }
+
+        // Copy the word
+        memcpy(&output[current_idx], word, word_len);
+        current_idx += word_len;
+    }
+
+    // Null-terminate
+    output[current_idx] = '\0';
+}
+
+// Turn off canonical mode + echo
+void enable_raw_mode(struct termios *old)
+{
+    struct termios new;
+    tcgetattr(STDIN_FILENO, old); // get current settings
+    new = *old;
+    new.c_lflag &= ~(ICANON | ECHO); // disable canonical mode & echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &new);
+}
+
+// Restore original terminal settings
+void disable_raw_mode(struct termios *old)
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, old);
+}
+
+void print_text(const char *text, int *failed_chars, int current_idx)
+{
+    int len = strlen(text);
+
+    printf("\033[2K\r"); // Clear the current line
+
+    for (int i = 0; i < len; i++)
+    {
+        if (i < current_idx)
+        {
+            if (failed_chars[i])
+            {
+                // Red for failed char, underscore if it was a space
+                if (text[i] == ' ')
+                    printf("\033[31m_\033[0m");
+                else
+                    printf("\033[31m%c\033[0m", text[i]);
+            }
+            else
+            {
+                printf("\033[32m%c\033[0m", text[i]); // Green for correct
+            }
+        }
+        else
+        {
+            printf("%c", text[i]); // Not yet typed
+        }
+    }
+
+    fflush(stdout);
+}
+
+int main(void)
+{
+    // Seed the random generator
+    srand(time(NULL));
+
+    int current_idx = 0;
+
+    char *words[NBR_WORDS];
+
+    int word_count = read_words("words.txt", words, NBR_WORDS);
+    if (word_count < 0)
+    {
+        return 1;
+    }
+
+    char text[1000];
+
+    build_test_text(words, NBR_WORDS, text, sizeof(text), NBR_TEXT_WORDS);
+
+    int text_len = strlen(text);
+
+    // Save terminal mode
+    struct termios old;
+    enable_raw_mode(&old);
+
+    // Keep track of the failed chars
+    int *failed_chars = calloc(text_len, sizeof(int));
+    if (!failed_chars)
+    {
+        perror("calloc failed");
+        return 1;
+    }
+
+    // Read stats for keys
+    key_stat key_stats[NUM_KEYS];
+    load_key_stats("stats/key_stats.txt", key_stats);
+
+    // Initial display
+    printf("%s", text);
+
+    // Start timer
+    struct timeval start, end, key_timer_start, key_timer_end;
+    gettimeofday(&start, NULL);
+    gettimeofday(&key_timer_start, NULL);
+
+    while (current_idx < text_len)
+    {
+        // Move cursor to current position
+        printf("\033[%dG", current_idx + 1);
+
+        // Read input
+        char input;
+        scanf("%c", &input);
+
+        if (input == text[current_idx])
+        {
+            gettimeofday(&key_timer_end, NULL);
+            double elapsed_sec_for_key = (key_timer_end.tv_sec - key_timer_start.tv_sec) +
+                                         (key_timer_end.tv_usec - key_timer_start.tv_usec) / 1e6;
+
+            // Add success or fail for key
+            if (input != ' ') // We don't keep track of space key stats
+            {
+                if (failed_chars[current_idx] == 0)
+                {
+                    key_stats[input - 'a'].successes++;
+                }
+                else
+                {
+                    key_stats[input - 'a'].fails++;
+                }
+                key_stats[input - 'a'].total_time += elapsed_sec_for_key;
+            }
+            current_idx++;
+
+            // Start timer for next key
+            gettimeofday(&key_timer_start, NULL);
+        }
+        else
+        {
+            failed_chars[current_idx] = 1;
+        }
+
+        print_text(text, failed_chars, current_idx);
+    }
+
+    // Stop timer
+    gettimeofday(&end, NULL);
+    double elapsed_sec = (end.tv_sec - start.tv_sec) +
+                         (end.tv_usec - start.tv_usec) / 1e6;
+
+    double nbr_words = (double)text_len / 5;
+    double words_per_minute = nbr_words / elapsed_sec * 60.0;
+
+    int nbr_fails = 0;
+    for (int i = 0; i < text_len; i++)
+    {
+        if (failed_chars[i] == 1)
+        {
+            nbr_fails++;
+        }
+    }
+
+    double accuracy = (double)(text_len - nbr_fails) / (double)text_len * 100.0;
+
+    save_key_stats("stats/key_stats.txt", key_stats);
+
+    disable_raw_mode(&old);
+    free(failed_chars);
+
+    printf("\nDone!\n");
+    printf("Speed: %.1fwpm\n", words_per_minute);
+    printf("Accuracy: %.1f%%\n", accuracy);
+}
